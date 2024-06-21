ff82c7a9042f49b5c15e704694ce57938a51141c
Eduardo Batista <72777045+eudevedu@users.noreply.github.com>
2024-06-21 15:41:24 -0500

    Update page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -15,7 +15,7 @@ const popup = L.popup({
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Educandário Santa Margarida <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent('Educandário Santa Margarida <a href="orphanage.html" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
 
 L
 .marker([-9.974057,-67.8212127], { icon })
